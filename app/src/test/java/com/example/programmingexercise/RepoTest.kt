c6ecaeea87bbbfb661cfa960b6c6bcc6acf2d0ac
Consultant <consultant@MBP-C02CV4LCMD6M-MADAWES.local>
2023-08-24 11:29:02 -0400

    update tests

--- app/src/test/java/com/example/programmingexercise/RepoTest.kt
+++ app/src/test/java/com/example/programmingexercise/RepoTest.kt
@@ -3,14 +3,17 @@ package com.example.programmingexercise
 import com.example.programmingexercise.data.CountryRepo
 import com.example.programmingexercise.data.local.Country
 import com.example.programmingexercise.data.remote.APIService
+import com.example.programmingexercise.data.remote.ResultState
 import com.example.programmingexercise.data.remote.dtos.CountryDTO
 import com.example.programmingexercise.util.CoroutinesExtension
 import io.mockk.mockk
 import io.mockk.coEvery
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
+import java.io.IOException
 
 internal class RepoTest {
 
@@ -33,16 +36,38 @@ internal class RepoTest {
                 region = "NA"
             )
         )
-        val expectedResult = listOf(
-            Country(
-                code = "US",
-                capital = "Washington, D.C.",
-                name = "United States of America",
-                region = "NA"
+        val expectedResult = ResultState.Success(
+            listOf(
+                Country(
+                    code = "US",
+                    capital = "Washington, D.C.",
+                    name = "United States of America",
+                    region = "NA"
+                )
             )
         )
+
         coEvery { service.getCountries() } coAnswers { serviceResponse }
+
         val result = repo.getCountries()
-        Assertions.assertEquals(expectedResult, result)
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun testGetCountriesError() = runTest(testExtension.testDispatcher) {
+        val exceptionMessage = "Network error"
+        val thrownException = IOException(exceptionMessage)
+
+        // Mocking a thrown exception for service.getCountries()
+        coEvery { service.getCountries() } coAnswers { throw thrownException }
+
+        val result = repo.getCountries()
+
+        // Check if the result is of type ResultState.Error
+        assertTrue(result is ResultState.Error)
+
+        // Cast the result to ResultState.Error and check if the exception message matches
+        val errorResult = result as ResultState.Error
+        assertEquals(exceptionMessage, errorResult.exception.message)
     }
 }
--- app/src/test/java/com/example/programmingexercise/ViewModelTest.kt
+++ app/src/test/java/com/example/programmingexercise/ViewModelTest.kt
@@ -2,13 +2,15 @@ package com.example.programmingexercise
 
 import com.example.programmingexercise.data.CountryRepo
 import com.example.programmingexercise.data.local.Country
+import com.example.programmingexercise.data.remote.ResultState
 import com.example.programmingexercise.presentation.CountryViewModel
 import com.example.programmingexercise.util.CoroutinesExtension
 import com.example.programmingexercise.util.InstantTaskExecutorExtension
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.RegisterExtension
@@ -18,12 +20,12 @@ internal class ViewModelTest {
 
     @RegisterExtension
     private val testExtension = CoroutinesExtension()
-    private val repo: CountryRepo = mockk()
+    private val repo: CountryRepo = mockk(relaxed = true)
     private val viewModel = CountryViewModel(repo)
 
     @Test
     fun testGetCountries() = runTest(testExtension.testDispatcher) {
-        val expectedResult = listOf(
+        val expectedData = listOf(
             Country(
                 code = "US",
                 capital = "Washington, D.C.",
@@ -31,9 +33,14 @@ internal class ViewModelTest {
                 region = "NA"
             )
         )
-        coEvery { repo.getCountries() } coAnswers { expectedResult }
+        val successResult = ResultState.Success(expectedData)
+        coEvery { repo.getCountries() } coAnswers { successResult }
+
         viewModel.getCountries()
+
         val result = viewModel.countries.value
-        Assertions.assertEquals(expectedResult, result)
+
+        assertTrue(result is ResultState.Success)
+        assertEquals(expectedData, (result as ResultState.Success).data)
     }
 }
